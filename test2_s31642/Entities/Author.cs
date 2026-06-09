4d45f38fde496420f73865adee9b7d4c6ab98311
Kira <s31642@pjwstk.edu.pl>
2026-06-09 10:23:17 +0200

    Initial Project

---
+++ test2_s31642/Entities/Author.cs
@@ -0,0 +1,8 @@
+namespace test2_s31642.Entities;
+
+public class Author
+{
+    public int AuthorId { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
